0663cd65f30af6c70a1c3a96e627876f39721417
Joel Warrington <joelw@hey.com>
2025-04-28 16:23:16 -0600

    Update logo and change new game link to a button

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
-import Logo from '@/components/Logo/Logo'
 import type { Route } from './+types/home'
 import { NavLink } from 'react-router'
+import Logo from '@/app/logo.svg'
+import { Button } from '@/components/ui/button'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,8 +18,10 @@ export default function Page() {
     <div className="grid justify-items-center">
       <h1 className="sr-only">Wizardly</h1>
       <p className="sr-only">A companion app for the Wizard card game.</p>
-      <Logo />
-      <NavLink to="/games/new">New game</NavLink>
+      <img src={Logo} alt="Logo of the Wizard card game" />
+      <Button asChild>
+        <NavLink to="/games/new">New game</NavLink>
+      </Button>
     </div>
   )
 }
